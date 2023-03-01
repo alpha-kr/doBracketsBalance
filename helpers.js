fc328473a8541705080d2c745bece75441c48634
alpha-kr <bandy@uninorte.edu.co>
2023-03-01 09:59:18 -0500

    ⚙ doBracketsBalance logic implemented

--- helpers.js
+++ helpers.js
@@ -1,5 +1,38 @@
 function doBracketsBalance(brackets) {
-    return false;
+    if (typeof brackets != "string") return  false;
+
+    if(brackets.length == 0) return false;
+
+    const openBrackets = ['[', '{', '('];
+
+    const closeBrackets = [']', '}', ')'];
+
+    const stack = [];
+
+    let isValidString = true;
+
+    let i = 0;
+
+    while (isValidString && i < brackets.length) {
+
+        let item = brackets[i];
+
+        if (openBrackets.includes(item)) {
+            stack.push(item);
+        }
+
+        if (closeBrackets.includes(item) && stack.pop() !== openBrackets[closeBrackets.indexOf(item)]) {
+            isValidString = false;
+        }
+
+        i++;
+    }
+
+    if (stack.length > 0) {
+        isValidString = false;
+    }
+
+    return isValidString;
 }
 
 module.exports = {doBracketsBalance};
